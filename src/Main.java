7646a219533c265d0c20ac8359ae8953b7d77921
Heaven <hcortez44359@liceo.edu.ph>
2025-08-08 09:43:35 +0800

    prelim-exam

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello world");
+        Student s1 = new Student("Rodrigo Duterte\n", 20, "BSIT", 80, 90, 87);
+        Student s2 = new Student("Bongbong Marcos\n", 19, "BSCS", 75, 65, 60);
+        Student s3 = new Student("Nicolas Deloso Torre III", 21, "BSIT", 84, 89, 92);
+
+        Student[] students = { s1, s2, s3 };
+        int passingCount = 0;
+
+        for (Student stdnts : students) {
+            stdnts.displayInfo();
+            if (stdnts.isPassing()) {
+                passingCount++;
+            }
+        }
+
+        System.out.println("Summary: " + passingCount + " out of " +
+                students.length + " students are passing.");
     }
 }
